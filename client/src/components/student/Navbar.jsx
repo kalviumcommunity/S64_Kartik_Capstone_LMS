331e7e3945078f2814ec4955252ee8bdc1db65c1
KARTIK001-CODER <0011kartikchoudhary@gmail.com>
2025-04-21 06:26:08 +0530

    Frontend

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,23 +1,79 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import { assets } from "../../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
-
   const location = useLocation();
-  const isCourseListPage = location.pathname.includes('/courses-list');
+  const isCourseListPage = location.pathname.includes("/courses-list");
+
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
-    <div className={`flex items-centor justify-between px-4 sm:px-10 md:py-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>  
-      <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
-      <div className='hidden md:flex items-center gap-5 text-gray-500' >  
-        <div>
-          <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollments</Link>
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
+        isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+      }`}
+    >
+      <img src={logo} alt="logo" className="w-28 lg:w-32 cursor-pointer" />
+
+      <div className="hidden md:flex items-center gap-5 text-gray-500">
+        <div className="flex items-center gap-5">
+          {user && (
+            <>
+              <button className="hover:text-blue-600 transition">
+                Become Educator
+              </button>
+              |{" "}
+              <Link
+                to="/my-enrollments"
+                className="hover:text-blue-600 transition"
+              >
+                My Enrollments
+              </Link>
+            </>
+          )}
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2'>Create Account</button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition"
+          >
+            Create Account
+          </button>
+        )}
+      </div>
+      <div className="md:hidden flex-items-center gap-2 sm:gap-5 text-gray-500">
+        <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+          {user && (
+            <>
+              <button className="hover:text-blue-600 transition">
+                Become Educator
+              </button>
+              |{" "}
+              <Link
+                to="/my-enrollments"
+                className="hover:text-blue-600 transition"
+              >
+                My Enrollments
+              </Link>
+            </>
+          )}
+          {user ? (
+            <UserButton />
+          ) : (
+            <button onClick={() => openSignIn()}>
+              <img src={assets.user_icon} alt="" />
+            </button>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
